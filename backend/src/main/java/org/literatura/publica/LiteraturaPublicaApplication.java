78b7dd31a1ebaee1298ddad49382ad35fb86cd08
Bruno Fagundes <brunogarciafagundes@gmail.com>
2025-01-06 18:30:19 -0300

    first commit

---
+++ backend/src/main/java/org/literatura/publica/LiteraturaPublicaApplication.java
@@ -0,0 +1,13 @@
+package org.literatura.publica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaPublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaPublicaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/literatura/publica/LiteraturaPublicaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.literatura.publica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaPublicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
