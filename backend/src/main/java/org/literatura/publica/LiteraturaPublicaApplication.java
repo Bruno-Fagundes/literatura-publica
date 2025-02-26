6786833e7a9ee07b3b0c51084328f15d75829db2
Bruno Fagundes <brunogarciafagundes@gmail.com>
2025-02-26 19:35:05 -0300

    Add DTO

--- backend/src/main/java/org/literatura/publica/LiteraturaPublicaApplication.java
+++ backend/src/main/java/org/literatura/publica/LiteraturaPublicaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraturaPublicaApplication {
+public class  LiteraturaPublicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraturaPublicaApplication.class, args);
